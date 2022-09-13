02dd964a95101d336e3492f283a0fa916cfaa1df
KosKazzz <xuzzzz@gmail.com>
2022-09-13 22:40:54 +0300

    initial:first task

---
+++ src/Basket.java
@@ -0,0 +1,106 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Basket /*implements Serializable*/ {
+
+    private int[] prices;
+    private String[] products;
+    private int[] countOfProducts;
+
+    public Basket(int[] prices, String[] products) {
+        this.prices = prices;
+        this.products = products;
+        this.countOfProducts = new int[products.length];
+    }
+
+    public int[] getCountOfProducts() {
+        return countOfProducts;
+    }
+
+    public void setCountOfProducts(int[] countOfProducts) {
+        this.countOfProducts = countOfProducts;
+    }
+
+    public void addToCart(int productNum, int amount) {
+        this.countOfProducts[productNum - 1] += amount;
+        this.saveTxt("basket.txt");
+    }
+
+    public void printCart() {
+        int count = 1;
+        int sum = 0;
+        for (int i = 0; i < products.length; i++) {
+            if (countOfProducts[i] != 0) {
+                System.out.println(count + ". " + products[i] + "-"
+                        + countOfProducts[i] + "шт. "
+                        + prices[i] + " руб/шт., "
+                        + (countOfProducts[i] * prices[i]) + " руб. в сумме;");
+                count++;
+                sum += countOfProducts[i] * prices[i];
+            }
+        }
+        System.out.println("  Итого: " + sum + " руб.");
+    }
+
+    void saveTxt(String filePathAndName) {
+        filePathAndName = ".\\" + filePathAndName;
+        File fileBasket = new File(filePathAndName);
+        try (FileWriter writer = new FileWriter(fileBasket, false)) {
+            for (int pr : prices) {
+                writer.write(Integer.toString(pr));
+                writer.append(' ');
+            }
+            writer.append('\n');
+            for (String prod : products) {
+                writer.write(prod);
+                writer.append(' ');
+            }
+            writer.append('\n');
+            for (int cnt : countOfProducts) {
+                writer.write(Integer.toString(cnt));
+                writer.append(' ');
+            }
+            writer.append('\n');
+        } catch (IOException ex) {
+            System.err.println("Something went wrong");
+        }
+    }
+
+    public static Basket loadFromTxtFile(String fileName) {
+        //fileName = ".\\" + fileName + ".txt";
+        File fileBasket = new File(fileName);
+        StringBuilder stringFromTxtBasket = new StringBuilder();
+        try (FileReader reader = new FileReader(fileBasket)) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                stringFromTxtBasket.append((char) c);
+            }
+        } catch (IOException ex) {
+            System.err.println("Не получается прочитать файл!");
+        }
+        String[] parts = stringFromTxtBasket.toString().split("\\n");
+        int[] pricesFromFile = new int[parts[0].split(" ").length];
+        String[] productsFromFile = new String[parts[1].split(" ").length];
+        int[] countOfProductsFromFile = new int[parts[2].split(" ").length];
+        for (int i = 0; i < pricesFromFile.length; i++) {
+            try {
+                pricesFromFile[i] = Integer.parseInt(parts[0].split(" ")[i]);
+                productsFromFile[i] = parts[1].split(" ")[i];
+                countOfProductsFromFile[i] = Integer.parseInt(parts[2].split(" ")[i]);
+            } catch (NumberFormatException ex) {
+                System.err.println("Не получается преобразовать инфорамцию из файла.");
+            }
+        }
+        Basket basket = new Basket(pricesFromFile, productsFromFile);
+        basket.setCountOfProducts(countOfProductsFromFile);
+        //test
+        /*System.out.println(" Тут  prices - " + Arrays.toString(pricesFromFile));
+        System.out.println(" Тут  products - " + Arrays.toString(productsFromFile));
+        System.out.println(" Тут  countOfProducts - " + Arrays.toString(countOfProductsFromFile));
+        basket.printCart();*/
+        return basket;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,85 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] products = {"Хлеб", "Молоко", "Мясо", "Картофель", "Конфета"};
+        int[] prices = {56, 79, 550, 65, 200};
+        Basket bs;
+        File basketIsHere = new File(".\\basket.txt");
+        if (basketIsHere.canRead()) {
+            bs = Basket.loadFromTxtFile(".\\basket.txt");
+            System.out.println("Ваша корзина : ");
+            bs.printCart();
+            System.out.println();
+        } else {
+            System.out.println("Ваша корзина пуста!");
+            bs = new Basket(prices, products);
+            System.out.println();
+        }
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i + 1) + ". " + products[i] + " - " + prices[i] + " руб/шт.");
+        }
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Выберите номер товара и количество или для выхода наберите \"end\" .");
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            if (input.isEmpty()) {
+                System.out.println("Надо что-нибудь ввести =)");
+            }
+            if (input.length() < 3) {
+                System.out.println("Надо вводить правильно(номер продукта пробел количество!)");
+                continue;
+            }
+            int nomOfProd;
+            int countOfProd;
+            if (input.contains(" ")) {
+                if (input.startsWith(" ") || input.endsWith(" ")) {
+                    System.out.println("Надо вводить правильно(номер продукта пробел количество!)");
+                } else {
+                    int countOfRegex = 0;
+                    for (int i = 1; i < input.length() - 1; i++) {
+                        if (input.charAt(i) == ' ') {
+                            countOfRegex++;
+                        }
+                    }
+                    if (countOfRegex >= 2) {
+                        System.out.println("Ввели лишнее.Надо вводить правильно(номер продукта пробел количество!)");
+                        continue;
+                    }
+                    String[] parts = input.split(" ");
+                    try {
+                        nomOfProd = Integer.parseInt(parts[0]); // productNum
+                        countOfProd = Integer.parseInt(parts[1]); //amount
+                    } catch (NumberFormatException e) {
+                        System.out.println("Надо вводить цифры...");
+                        continue;
+                    }
+                    if (nomOfProd > products.length || nomOfProd <= 0) {
+                        System.out.println("Не верный номер продукта!");
+                        continue;
+                    }
+                    if (countOfProd > 100) {
+                        System.out.println(" Введено не приемлемо большое количество продуктов!");
+                        continue;
+                    }
+                    if (countOfProd == 0) {
+                        bs.getCountOfProducts()[nomOfProd - 1] = 0;
+                    } else {
+                        if (bs.getCountOfProducts()[nomOfProd - 1] + countOfProd < 0) {
+                            System.out.println("В корзине не может быть отрицательное количество!!!");
+                        } else {
+                            bs.addToCart(nomOfProd, countOfProd);
+                        }
+                    }
+                }
+            } else {
+                System.out.println("Необходимо номер продукта и количество разделить пробелом!");
+            }
+        }
+        bs.printCart();
+    }
+}
